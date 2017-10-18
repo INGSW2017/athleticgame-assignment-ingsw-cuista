5033c4b39ac9a10a34dac6b4d488fb73a8f87385
cuista <carlo.cristarellaorestano@gmail.com>
2017-10-18 12:59:57 +0200

    Esercizio Readme svolto

--- AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
+++ AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
@@ -1,5 +1,35 @@
 package it.unical.test;
 
-public class AthleticGameBasicTest {
+import java.time.Instant;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import it.unical.asde.AthleticGame;
+
+public class AthleticGameBasicTest
+{
+
+	private static AthleticGame game;
+
+	@BeforeClass
+	public static void init()
+	{
+		game = new AthleticGame("Tennis");
+	}
+
+	@Test
+	public void addArrivalTest()
+	{
+		game.addArrival("Stincio", Instant.now());
+		Assert.assertEquals(1, game.getArrivals().size());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void exceptionPartecipantTimeTest()
+	{
+		game.getParecipiantTime("Stincio");
+	}
 
 }
